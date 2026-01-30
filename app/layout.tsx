1558a4080683e8f56e58958805daabe0a1cd97d1
Gilang Ramadhan <45790050+9Alpha9@users.noreply.github.com>
2026-01-30 22:18:37 +0700

    Feat: Update minor (Favicon not change Google Search Engine)

--- app/layout.tsx
+++ app/layout.tsx
@@ -89,14 +89,14 @@ export const metadata: Metadata = {
 
   icons: {
     icon: [
-      { url: "/favicon.ico" },
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.ico?v=2", type: "image/x-icon" },
+      { url: "/favicon-16x16.png?v=2", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png?v=2", sizes: "32x32", type: "image/png" },
     ],
-    apple: [{ url: "/apple-touch-icon.png" }],
+    apple: [{ url: "/apple-touch-icon.png?v=2" }],
     other: [
-      { rel: "android-chrome-192x192", url: "/android-chrome-192x192.png" },
-      { rel: "android-chrome-512x512", url: "/android-chrome-512x512.png" },
+      { rel: "android-chrome-192x192?v=2", url: "/android-chrome-192x192.png" },
+      { rel: "android-chrome-512x512?v=2", url: "/android-chrome-512x512.png" },
     ],
   },
 
